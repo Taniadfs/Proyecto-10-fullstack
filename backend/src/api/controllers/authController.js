6d97cff6240ac2fc96cf0bad86a87f3eebbfb12e
Taniadfs <t.dangelofonfria@gmail.com>
2026-06-08 21:33:53 +0200

    routes actualizadas y controllers en curso

--- backend/src/api/controllers/authController.js
+++ backend/src/api/controllers/authController.js
@@ -1,12 +1,37 @@
-const express = require ('express') 
-const router = express.Router()    
+const Usuario= require('../models/usuario')
+const bcrypt = require ('bcrypt')
+const jwt = require ('jsonwebtoken')
 
-const {
-  register,
-  login,  
-} = require ('../controllers/authController')
+const register = async(req, res) =>{
+  try{ 
+    const {nombre, correo, contraseña} = req.body
+    if (!nombre || !correo || !contraseña) {
+      return res.status(400).json({ message: 'Todos los campos son obligatorios' })
+    } 
+    if (!contraseña || contraseña.length < 8){
+      return res.status(400).json({ message: 'La contraseña debe tener al menos 8 caracteres' })
+    }
+    if (!/^\S+@\S+\.\S+$/.test(correo)){
+      return res.status(400).json({ message: 'El correo debe ser una dirección válida' })
+    }
+    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(contraseña)) {
+      return res.status(400).json({
+        message:
+          'La contraseña debe tener una mayúscula, una minúscula y un número'
+      })
+    }
 
-router.post('/register', register)
-router.post ('/login', login)
+    const emailExiste = await Usuario.findOne({correo : correo})
+    if (emailExiste) {
+      return res.status(409).json({ message: 'El usuario ya existe' })
+    }
+    const hashedContraseña = await bcrypt.hash(contraseña, 10)
 
-module.exports=router
+
+
+    const nuevoUsuario = new Usuario({nombre, correo, contraseña})
+  }
+  catch (error){
+    res.status(500).json({message: 'Error en el servidor'})
+  }
+}
