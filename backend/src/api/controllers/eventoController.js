edcd6cb02ead8235faa6e547901aa411734002dc
Taniadfs <t.dangelofonfria@gmail.com>
2026-06-18 21:23:04 +0200

    eventcontroller ok

--- backend/src/api/controllers/eventoController.js
+++ backend/src/api/controllers/eventoController.js
@@ -23,3 +23,28 @@ const getEventosById=async(req,res)=>{
     res.status(500).json({message:'Error en el servidor'})
   }
 }
+
+const createEvento=async(req,res)=>{
+  try{ 
+    const {titulo, fecha, lugar, descripcion, imagen}= req.body
+    if (titulo === undefined || fecha === undefined || lugar === undefined) {
+      return res.status(400).json({message:'Faltan datos obligatorios'})
+    }
+    const eventoExistente= await Evento.findOne({titulo, fecha})
+    if(eventoExistente){
+     return res.status(409).json({message:'Ya existe un evento con el mismo titulo y fecha'})}
+
+
+    const nuevoEvento= new Evento({titulo, fecha, lugar, descripcion, imagen})
+    await nuevoEvento.save()
+    
+    res.status(201).json({message:'Evento creado exitosamente', evento:nuevoEvento})
+
+
+
+  }
+
+catch(error){
+  
+      res.status(500).json({message:'Error en el servidor'})
+}}
