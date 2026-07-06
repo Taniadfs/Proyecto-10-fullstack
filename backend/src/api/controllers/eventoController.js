8892595f2408afc9a704572bcefda83134a08bc9
Taniadfs <t.dangelofonfria@gmail.com>
2026-07-06 21:04:18 +0200

    eventoController completado

--- backend/src/api/controllers/eventoController.js
+++ backend/src/api/controllers/eventoController.js
@@ -48,3 +48,60 @@ catch(error){
   
       res.status(500).json({message:'Error en el servidor'})
 }}
+
+const updateEvento=async(req,res)=>{
+  try{
+    const {titulo, fecha, lugar, descripcion, imagen}= req.body
+    const evento= await Evento.findById(req.params.id)
+    if(!evento){
+      return res.status(404).json({message:'Evento no encontrado'})
+    }
+    evento.titulo=titulo || evento.titulo
+    evento.fecha=fecha || evento.fecha
+    evento.lugar=lugar || evento.lugar
+    evento.descripcion=descripcion || evento.descripcion
+    evento.imagen=imagen || evento.imagen
+
+    await evento.save()
+    res.status(200).json({message:'Evento actualizado exitosamente',evento})
+
+  }
+  catch(error){
+    res.status(500).json({message:'Error en el servidor'})
+  }
+}
+const deleteEvento=async(req,res)=>{
+  try{
+    const evento= await Evento.findByIdAndDelete(req.params.id)
+    if(!evento){
+      return res.status(404).json({message:'Evento no encontrado'})
+    }
+    
+    res.status(200).json({message:'Evento eliminado exitosamente'})
+  }
+  catch(error){
+    res.status(500).json({message:'Error en el servidor'})
+  }
+} 
+
+const addAsistente=async(req,res)=>{
+  try{
+    const {idEvento, idUsuario}=req.params
+    const evento= await Evento.findById(idEvento)
+    if(!evento){
+      return res.status(404).json({message:'Evento no encontrado'})
+    }
+    if (evento.asistentes.some(asistente => asistente.toString() === idUsuario)){
+      
+      return res.status(400).json({message:'El usuario ya es asistente del evento'})
+    }
+    evento.asistentes.push(idUsuario)
+    await evento.save()
+    res.status(200).json({message:'Asistente agregado exitosamente',evento})
+  }
+  catch(error){
+    res.status(500).json({message:'Error en el servidor'})
+  }
+}
+
+module.exports={getEventos,getEventosById,createEvento,updateEvento,deleteEvento,addAsistente}
