8a1b371bcca981a24f7fea82474208c57904061e
Taniadfs <t.dangelofonfria@gmail.com>
2026-08-24 20:30:05 +0200

    inicio de index.js

---
+++ frontend/js/index.js
@@ -0,0 +1,7 @@
+import { saveToken } from "./auth.js";
+import apiFetch from "./fetch.js";
+
+const loginForm= document.querySelector('#formLogin')
+const loginRegistrer= document.querySelector('#formRegistrer')
+const linkRegister= document.querySelector('#registrarse')
+const linkLogin= document.querySelector('#irALogin')
